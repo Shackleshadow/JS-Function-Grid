8e780189c51bf7c60674b3ce8b0f39edcb1ad66d
Bryan Huang <chesschamp617@gmail.com>
2017-02-13 15:18:04 +0000

    Pixel Design

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //Write your function calls below this line
+/*
+setColor(0, 0, "blue");
+setColor(0, 9, "blue");
+setColor(9, 0, "blue");
+setColor(9, 9, "blue");
+*/
+
+/*
+setColor(2, 2, "blue");
+setColor(2, 4, "blue");
+setColor(4, 1, "blue");
+setColor(5, 2, "blue");
+setColor(5, 3, "blue");
+setColor(5, 4, "blue");
+setColor(4, 5, "blue");
+*/
+
+setColor(0, 0, "blue");
+setColor(0, 1, "blue");
+setColor(0, 2, "blue");
+setColor(1, 0, "blue");
+setColor(2, 0, "blue");
+setColor(2, 0, "blue");
+setColor(2, 1, "blue");
+setColor(2, 2, "blue");
+setColor(3, 2, "blue");
+setColor(4, 2, "blue");
+setColor(4, 1, "blue");
+setColor(4, 0, "blue");
